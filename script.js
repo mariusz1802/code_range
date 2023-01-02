b2f56f80bb4ec1a0b909014531fd213080c04d9b
Mariusz <mariusz.janowski@yahoo.pl>
2023-01-02 20:47:01 +0100

    async symulating with CALLBACK

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const login = "a"
-const password = "b"
+console.log("Start");
+
+//instead of giving return I gave callback function
+//thanks callback function we can handle asynchronious function
+
+const login = (name, password, callback) => {
+    setTimeout(() => {
+        console.log("Now we  have the data")
+        callback({userName : name})
+    }, 2000);
+}
+
+
+const newUser = login("TomaszChic", 12345, (user)=> { console.log(user)});
 
 
 
-const logowanie = new Promise((resolve,reject) => {
-    resolve("Zalogowałes sie poprawnie");
-    reject("Nie udało sie zalogowac")
-})
-
-
-
-logowanie.then((successMessage) => {
-            console.log(`Jest! ${successMessage}`)
-}).error(`Incorrect login or password`);
+console.log("End");
