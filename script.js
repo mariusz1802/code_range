299ea040ac8c168c43bed275b838f1975471128f
Mariusz <mariusz.janowski@yahoo.pl>
2023-01-06 13:58:02 +0100

    Composition with Car- factory function - exercise

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
-const recordable = (state) => ({
-  record : () => `I'm recordin a new ${state.type}!`,
-})
+// const recordable = (state) => ({
+//   record: () => `I'm recordin a new ${state.type}!`,
+// });
 
-const sharable = (state) => ({
-share : () => `Spread a word about ${state.name}...`,
-})
+// const sharable = (state) => ({
+//   share: () => `Spread a word about ${state.name}...`,
+// });
 
-const watchable = (state) =>({
-  watch : () => `I'm watching right now: ${state.name}`
-})
+// const watchable = (state) => ({
+//   watch: () => `I'm watching right now: ${state.name}`,
+// });
 
-const movie = (name) => {
-  const state = {name : name, type: 'movie'}
-    return Object.assign({}, recordable(state), sharable(state), watchable(state));
-}
+// const movie = (name) => {
+//   const state = { name: name, type: "movie" };
+//   return Object.assign(
+//     {},
+//     recordable(state),
+//     sharable(state),
+//     watchable(state)
+//   );
+// };
 
+// const latestMovie = movie("Kompozycja vs. Dziedziczenie");
 
+// latestMovie.record(); //I'm recording a new movie!
+// latestMovie.share(); //Spread a word about Kompozycja vs. Dziedziczenie ...
+// latestMovie.watch(); //I'm watching right now: Kompozycja vs. dziedziczenie
 
-const latestMovie = movie('Kompozycja vs. Dziedziczenie');
+const nameable = (state) => ({
+  showName: () => {
+    `This car name is ${state.name}`;
+  },
+});
 
+const speedable = (state) => ({
+  showSPeed: () => {
+    `${state.name} is quite fast: ${state.speed}`;
+  },
+});
 
-latestMovie.record(); //I'm recording a new movie!
-latestMovie.share(); //Spread a word about Kompozycja vs. Dziedziczenie ...
-latestMovie.watch() //I'm watching right now: Kompozycja vs. dziedziczenie
+const gearable = (state) => ({
+  gearAmount: () => {
+    `${state.name}  is gearbox ${state.gearbox}`;
+  },
+});
+
+const car = () => {
+  const state = { name: name, speed: speed, gearbox: gearbox };
+  return Object.assign({}, nameable(state), speedable(state), gearable(state));
+};
+
+const volvo = car("Volvo", "240km/h", "7");
+
+volvo.gearAmount();
