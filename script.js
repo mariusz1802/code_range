65aa8fb0d1b4f79f772423afcf5e66a4092e9fb1
Mariusz <mariusz.janowski@yahoo.pl>
2023-03-06 19:01:02 +0100

    async await recap

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-//Code without NullObject pattern:
-/*We need a lot of if statments to check is user exist thats can provide additional problems on the web.
- scriptNullUser.js is file with same example but with NullObject pattern */
-class User {
-  constructor(id, name) {
-    this.id = id;
-    this.name = name;
-  }
-  hasAccess() {
-    return this.name === "Bob";
-  }
+console.log("start");
+
+function LoginUser(name, password) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ name: name, password: password });
+    }, 2000);
+  });
 }
 
-const users = [new User(1, "Bob"), new User(2, "John")];
+LoginUser("Mariusz", 1234)
+  .then((user) => {
+    console.log(user.name);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-function getUser(id) {
-  return users.find((user) => user.id === id);
-}
-
-function printUser(id) {
-  const user = getUser(id);
-  let name = "Guest";
-  if (user != null && user.name != null) name = user.name;
-  console.log("Hello " + name);
-
-  if (user != null && user.hasAccess != null && user.hasAccess()) {
-    console.log("yOU HAVE ACCESSS");
-  } else {
-    console.log("You are not allowed here");
-  }
-}
+console.log("end");
